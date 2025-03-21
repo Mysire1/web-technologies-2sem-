8c2c34bdc1c78f86e81f95da2432849d7000f782
Kostyantyn Smozhevskykh <lee.ross2005@gmail.com>
2025-03-21 13:09:13 +0200

    lab3JS completed

---
+++ lab3JS/task.js
@@ -0,0 +1,14 @@
+function fibonacciNumbers () {
+    let n1 = 0;
+    let n2 = 1;
+    let count = 0;
+    do {
+        console.log(n1)
+        let next = n1 + n2;
+        n1 = n2;
+        n2 = next;
+        count++;
+    }while (count <= 10)
+}
+
+console.log(fibonacciNumbers())
