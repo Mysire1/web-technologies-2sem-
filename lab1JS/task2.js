0e7eef93345531e2123f424849c32faa1c03f13d
Kostyantyn Smozhevskykh <lee.ross2005@gmail.com>
2025-04-06 16:07:24 +0300

    Fixed tasks and add gitignore file

--- lab1JS/task2.js
+++ lab1JS/task2.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const clickname = document.getElementById("StudentName")
 
 clickname.onmouseover = function () {
-    alert("Ім я студента Костянтин Сможевських ")
-=======
-const clickname = document.getElementById("StudentName")
-
-clickname.onmouseover = function () {
-    alert("Ім я студента Костянтин Сможевських ")
->>>>>>> 25363878f567e4708c220af38a7cd5bd3d26b964
+    alert("Ім'я студента Костянтин Сможевських ")
 }
